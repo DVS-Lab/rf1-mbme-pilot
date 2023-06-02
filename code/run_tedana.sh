d7f0b81943ad97535e87ef2f6a38b80aa43fb126
DVSneuro <david.v.smith@temple.edu>
2023-06-01 23:55:36 -0400

    reduce cores

--- code/run_tedana.sh
+++ code/run_tedana.sh
@@ -1,2 +1,2 @@
 #scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-python my_tedana.py --fmriprepDir /data/projects/rf1-mbme-pilot/derivatives/fmriprep --bidsDir /data/projects/rf1-mbme-pilot/bids --cores 40
+python my_tedana.py --fmriprepDir /data/projects/rf1-mbme-pilot/derivatives/fmriprep --bidsDir /data/projects/rf1-mbme-pilot/bids --cores 10
