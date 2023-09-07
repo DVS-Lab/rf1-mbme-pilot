87878e33400a1e33de01845010f5b2e8a982d6f8
DVSneuro <david.v.smith@temple.edu>
2023-09-07 16:38:40 -0400

    initial commit

--- code/fmriprep-hpc.sh
+++ code/fmriprep-hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -l walltime=16:00:00
+#PBS -l walltime=24:00:00
 #PBS -N fmriprep-all
 #PBS -q normal
-#PBS -m bae
+#PBS -m ae
 #PBS -M david.v.smith@temple.edu
 #PBS -l nodes=12:ppn=4
 
@@ -11,11 +11,9 @@ module load fsl/6.0.2
 source $FSLDIR/etc/fslconf/fsl.sh
 module load singularity/3.8.5
 cd $PBS_O_WORKDIR
-#echo "working in $PBS_O_WORKDIR with OMP_NUM_THREADS=$OMP_NUM_THREADS"
 
-#export OMP_NUM_THREADS=7
 # ensure paths are correct
-maindir=~/work/rf1-mbme-pilot
+maindir=~/work/rf1-mbme-pilot #this should be the only line that has to change if the rest of the script is set up correctly
 scriptdir=$maindir/code
 bidsdir=$maindir/bids
 logdir=$maindir/logs
@@ -27,12 +25,11 @@ touch $logdir/cmd_fmriprep_${PBS_JOBID}.txt
 
 # make derivatives folder if it doesn't exist.
 # let's keep this out of bids for now
-test=-02
-if [ ! -d $maindir/derivatives$test ]; then
-	mkdir -p $maindir/derivatives$test
+if [ ! -d $maindir/derivatives ]; then
+	mkdir -p $maindir/derivatives
 fi
 
-scratchdir=~/scratch/fmriprep$test
+scratchdir=~/scratch/fmriprep
 if [ ! -d $scratchdir ]; then
 	mkdir -p $scratchdir
 fi
@@ -42,26 +39,44 @@ MPLCONFIGDIR_DIR=~/work/mplconfigdir
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
 export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
 
-for sub in `ls -1d $bidsdir/sub-*`; do
-#for sub in sub-10438 sub-10422 sub-10391; do
-	sub="${sub##*/}"
-
-	echo singularity run --cleanenv \
-	-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
-	-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
-	-B $maindir:/base \
-	-B ~/work/tools/licenses:/opts \
-	-B $scratchdir:/scratch \
-	~/work/tools/fmriprep-22.0.2.simg \
-	/base/bids /base/derivatives$test/fmriprep \
-	participant --participant_label $sub \
-	--stop-on-first-crash \
-	--nthreads 12 \
-	--me-output-echos \
-	--stop-on-first-crash \
-	--fs-no-reconall --fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
-
+for sub in `cat ${scriptdir}/newsubs_rf1-mbme-pilot.txt` ; do
+	# did any participants get out of the scanner during the sequence pilot? the subs below are for th emain study
+	if [ $sub -eq 10317 ] || [ $sub -eq 10369 ] || [ $sub -eq 10402 ] || [ $sub -eq 10486 ] || [ $sub -eq 10541 ] || [ $sub -eq 10572 ] || [ $sub -eq 10584 ] || [ $sub -eq 10589 ] || [ $sub -eq 10691 ] || [ $sub -eq 10701 ]; then
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--use-syn-sdc \
+		--cifti-output 91k \
+		--output-spaces fsLR fsaverage MNI152NLin6Asym \
+		--bids-filter-file /base/code/fmriprep_config.json \
+		--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	else
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--cifti-output 91k \
+		--output-spaces fsLR fsaverage MNI152NLin6Asym \
+		--bids-filter-file /base/code/fmriprep_config.json \
+		--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	fi
 done
-# --nthreads 28 --omp-nthreads 7 \
 
 torque-launch -p $logdir/chk_fmriprep_${PBS_JOBID}.txt $logdir/cmd_fmriprep_${PBS_JOBID}.txt
