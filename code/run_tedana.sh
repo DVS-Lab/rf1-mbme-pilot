3b80f49af1e493f7eb55bdbddb7e21ead06392c9
DVSneuro <david.v.smith@temple.edu>
2023-09-23 18:21:55 -0400

    change inputs

--- code/run_tedana.sh
+++ code/run_tedana.sh
@@ -1,2 +1,3 @@
 #scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-python my_tedana.py --fmriprepDir /data/projects/rf1-mbme-pilot/derivatives/fmriprep --bidsDir /data/projects/rf1-mbme-pilot/bids --cores 10
+sub=$1
+python tedana-multi.py --fmriprepDir /data/projects/rf1-mbme-pilot/derivatives/fmriprep/sub-${sub} --bidsDir /data/projects/rf1-mbme-pilot/bids/sub-${sub}
