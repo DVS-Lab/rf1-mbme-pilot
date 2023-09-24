0620ecf6528a82c6d6febdeac950f4bebb3a0851
David V. Smith <david.v.smith@temple.edu>
2023-09-24 13:55:19 -0400

    separate output for tedana

--- code/run_tedana.sh
+++ code/run_tedana.sh
@@ -1,6 +1,7 @@
-#scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#!/bin/bash
+
 sub=$1
 python tedana-multi.py \
---fmriprepDir /data/projects/rf1-mbme-pilot/derivatives/fmriprep/sub-${sub} \
---bidsDir /data/projects/rf1-mbme-pilot/bids/sub-${sub} \
+--fmriprepDir /ZPOOL/data/projects/rf1-mbme-pilot/derivatives/fmriprep/sub-${sub} \
+--bidsDir /ZPOOL/data/projects/rf1-mbme-pilot/bids/sub-${sub} \
 --sub $sub
--- code/tedana-multi.py
+++ code/tedana-multi.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import json
 import os
 import re
-
 import pandas as pd
 from tedana import workflows
 import time
-
 import argparse
 
+
+# define function
+def RUN_Tedana(sub,prefix,EchoFiles,EchoTimes,OutDir):
+	if os.path.exists("%s/%s_desc-optcomDenoised_bold.nii.gz "%(OutDir,acq)):
+		print('Tedana was previously run for Sub %s remove directory if they need to be reanalyzed'%(sub))
+	else: 	
+		os.makedirs(OutDir,exist_ok=True)
+		print(OutDir)
+		print(acq)
+		print("Running TEDANA for %s"%(acq)+'\n')
+		
+		workflows.tedana_workflow(
+		EchoFiles, EchoTimes, out_dir=OutDir, prefix="%s"%(prefix), fittype="curvefit", tedpca="aic", gscontrol=None)
+	    
+	    # t2smap=fmriprep/func/sub-10188_task-sharedreward_acq-mb3me4_space-MNI152NLin6Asym_T2starmap.nii.gz
+
+	    #verbose=True,
+	    
+
+
+# collect inputs
 parser = argparse.ArgumentParser(
     description='Give me a path to your fmriprep output and number of cores to run')
 parser.add_argument('--fmriprepDir',default=None, type=str,help="This is the full path to your fmriprep dir")
 parser.add_argument('--bidsDir',default=None, type=str,help="This is the full path to your BIDS directory")
 parser.add_argument('--sub',default=None, type=int, help="This is the subject number.")
-
 args = parser.parse_args()
-#inputs
-
 prep_data = args.fmriprepDir
 bids_dir = args.bidsDir
 sub = args.sub
@@ -31,14 +43,13 @@ sub = args.sub
 echo_images=[f for root, dirs, files in os.walk(prep_data)
              for f in files if ('_echo-' in f)& (f.endswith('_bold.nii.gz'))]
 
+
 #Make a list of filenames that match the prefix
 image_prefix_list=[re.search('(.*)_echo-',f).group(1) for f in echo_images]
 image_prefix_list=set(image_prefix_list)
 
-print(image_prefix_list)
 
-#Make a dataframe where C1 is Sub C2 is inputFiles and C3 is Echotimes
-data=[]
+# run tedana on each set of multi-echo images
 for acq in image_prefix_list:
     print(acq)
     #Use RegEx to find Sub
@@ -60,50 +71,9 @@ for acq in image_prefix_list:
     confounds_file=[os.path.join(root, f) for root, dirs, files in os.walk(prep_data) for f in files
               if (acq in f) & (f.endswith('_desc-confounds_timeseries.tsv'))]
 
-    out_dir= os.path.join(
-        os.path.abspath(
-            os.path.dirname( prep_data )), "tedana/%s"%(sub))
-
-    #print(prep_data,out_dir)
-
-    data.append([sub,acq,acq_image_files,echo_times,out_dir])
+    out_dir = os.path.join(os.path.abspath(os.path.join(os.path.dirname(bids_dir), '..', 'derivatives')), 'tedana/%s/%s'%(sub,acq))
 
 
 
-#Changes can be reasonably made to
-#fittype: 'loglin' is faster but maybe less accurate than 'curvefit'
-#tedpca:'mdl'Minimum Description Length returns the least number of components (default) and recommeded
-#'kic' Kullback-Leibler Information Criterion medium aggression
-# 'aic' Akaike Information Criterion least aggressive; i.e., returns the most components.
-#gscontrol: post-processing to remove spatially diffuse noise. options implemented here are...
-#global signal regression (GSR), minimum image regression (MIR),
-#But anatomical CompCor, Go Decomposition (GODEC), and robust PCA can also be used
-
-
-def RUN_Tedana(sub,prefix,EchoFiles,EchoTimes,OutDir):
-
-    if os.path.exists("%s/%s_desc-optcomDenoised_bold.nii.gz "%(OutDir,acq)):
-        print('Tedana was previously run for Sub %s remove directory if they need to be reanalyzed'%(sub))
-    else:
-
-        os.makedirs(OutDir,exist_ok=True)
-        print("Running TEDANA for %s"%(acq)+'\n')
-
-        workflows.tedana_workflow(
-	    EchoFiles,
-	    EchoTimes,
-	    out_dir=OutDir,
-	    prefix="%s"%(prefix),
-	    fittype="curvefit",
-	    tedpca="kic",
-	    verbose=True,
-	    gscontrol=None)
-
-
-
-#print(acq_image_files)
-
-out_dir = os.path.join(os.path.abspath(os.path.dirname( prep_data )), "tedana/sub-%s"%(sub))
-
-
-#RUN_Tedana(sub,acq,acq_image_files,echo_times,out_dir)
+    
+    RUN_Tedana(sub,acq,acq_image_files,echo_times,out_dir)
