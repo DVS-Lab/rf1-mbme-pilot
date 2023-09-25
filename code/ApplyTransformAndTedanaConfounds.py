c4d1f2fed616a78472c8c1fd8aff0209839d9a48
DVSneuro <david.v.smith@temple.edu>
2023-09-24 21:43:30 -0400

    remove extraneuous

--- code/ApplyTransformAndTedanaConfounds.py
+++ code/ApplyTransformAndTedanaConfounds.py
@@ -1,66 +1,8 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
 import ants
 import matplotlib.pyplot as plt
-import os
-import re
-cores=4
-
-
-# In[2]:
-
-
-fmriprep_dir='../derivatives/fmriprep/'
-tedana_dir='../derivatives/tedana/'
-
-bold_imgs=[os.path.join(root,f) for root,dirs,files in os.walk(tedana_dir) for f in files if f.endswith('desc-optcom_bold.nii.gz')]
-
-
-# In[3]:
-
-
-def auto_antstransform(img):
-    sub='sub-'+re.search('/sub-(.*)/sub',img).group(1)
-    task=re.search('task-(.*)_acq',img).group(1)
-    acq=re.search('acq-(.*)_desc',img).group(1)
-    
-
-    output='%s/%s/func/%s_task-%s_acq-%s_desc-optcom-dewarped_bold.nii.gz'%(fmriprep_dir,sub,sub,task,acq)
-
-    if not os.path.isfile(output):
-        print('Applying Transforms: \n',sub,'task: '+task,'acquisition: '+acq)
-        fixed = ants.image_read(
-            '../masks/bg_image.nii') 
-        moving = ants.image_read( 
-            img)
-        reg_bold2T1w='%s/%s/func/%s_task-%s_acq-%s_from-scanner_to-T1w_mode-image_xfm.txt'%(
-            fmriprep_dir,sub,sub,task,acq)
-        reg_t1w2MNI='%s/%s/anat/%s_from-T1w_to-MNI152NLin2009cAsym_mode-image_xfm.h5'%(
-            fmriprep_dir,sub,sub)
-
-        #mywarpedimage = ants.apply_transforms( fixed=fixed, moving=moving,imagetype=3,
-                                                   #transformlist=[reg_bold2T1w,reg_t1w2MNI] )
-        #mywarpedimage.to_filename(output)
-    else:
-        print(output+' already exists please remove if this nees to be re-run')
-
-
-# In[4]:
-
-
-from multiprocessing import Pool
-
-#pool = Pool(cores)
-#results = pool.map(auto_antstransform, bold_imgs)
-
-
-# In[ ]:
-
-
 import os
 import pandas as pd
 from natsort import natsorted
@@ -68,7 +10,10 @@ import re
 import numpy as np
 
 
-# In[5]:
+fmriprep_dir='../derivatives/fmriprep-syn/'
+tedana_dir='../derivatives/tedana/'
+
+bold_imgs=[os.path.join(root,f) for root,dirs,files in os.walk(tedana_dir) for f in files if f.endswith('desc-optcom_bold.nii.gz')]
 
 
 metric_files = natsorted([os.path.join(root,f) for root,dirs,files in os.walk(
@@ -82,9 +27,9 @@ for file in metric_files:
     sub=re.search("tedana/(.*)/sub-",file).group(1)
     acq=re.search("acq-(.*)_desc",file).group(1)
     #print(sub,acq)
-    
+
     #import the data as dataframes
-    fmriprep_fname="../derivatives/fmriprep/%s/func/%s_task-sharedreward_acq-%s_desc-confounds_timeseries.tsv"%(sub,sub,acq)
+    fmriprep_fname="../derivatives/fmriprep-syn/%s/func/%s_task-sharedreward_acq-%s_desc-confounds_timeseries.tsv"%(sub,sub,acq)
     if os.path.exists(fmriprep_fname):
         print("Making Counfounds: %s %s"%(sub,acq))
         fmriprep_confounds=pd.read_csv(fmriprep_fname,sep='\t')
@@ -113,10 +58,3 @@ for file in metric_files:
         confounds_df.to_csv(outfname,index=False,header=False,sep='\t')
     else:
         print("fmriprep failed for %s %s"%(sub,acq))
-
-
-# In[ ]:
-
-
-
-
