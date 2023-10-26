37af5dfd048f685aa62d77c6528c5b8b09561dd5
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-25 23:06:55 -0400

    update for seq pilot 2.0

--- code/run_motioneval.sh
+++ code/run_motioneval.sh
@@ -11,40 +11,70 @@ for sub in `ls -d ${basedir}/bids/sub-*/`; do
 
   sub=${sub#*sub-}
   sub=${sub%/}
-  echo ${sub}  
+  echo ${sub}
 
-  for mb in 1 3 6; do
-	for me in 1 4; do
-	    if [[ $me -gt 1 ]]
+  if [ "${sub}" == "10589sp" ] || [ "${sub}" == "10590sp" ] || [ "${sub}" == "10603sp" ] || [ "${sub}" == "10606sp" ] || [ "${sub}" == "10608sp" ] || [ "${sub}" == "10644sp" ] || [ "${sub}" == "10690sp" ] || [ "${sub}" == "10691sp" ] || [ "${sub}" == "10723sp" ] || [ "${sub}" == "10741sp" ] || [ "${sub}" == "10777sp" ] || [ "${sub}" == "10803sp" ]; then
+    for mbme in mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50; do
+	    if [[ $mbme -gt mb3me1fa50 ]]
 	    then
-		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-mb${mb}me4_echo-2_bold.nii.gz
+		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-${mbme}_echo-2_bold.nii.gz
 	    else
-		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-mb${mb}me1_bold.nii.gz
+		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-${mbme}_bold.nii.gz
 	    fi
-	
-	#	
+
 	#reffile=sub-${basedir)/bids/sub-${sub}/func/${sub}_task-sharedreward_acq-mb${mb}me4_echo-1_part-mag_sbref.nii.gz
 
   	# Manages the number of jobs and cores
-	OUT=${basedir}/derivatives/fsl/mcflirt/sub-${sub}/mb${mb}me${me}/
+	OUT=${basedir}/derivatives/fsl/mcflirt/sub-${sub}/${mbme}/
 	if [ -d $OUT ]
         then
-            echo "sub ${sub} mb ${mb} me ${me} already has mclfirt"
+            echo "sub ${sub} mb ${mb} me ${me} already has mcflirt"
 	else
-	
 
 	    mkdir -p $OUT
-        
+
   	    NCORES=15
   	    while [ $(ps -ef | grep -v grep | grep 'mcflirt' | wc -l) -ge $NCORES ]; do
     		sleep 5s
   	    done
-  	    /usr/share/fsl/6.0.4/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
+  	    /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
             echo $BIDSDATA $OUT &
 		    sleep 1s
         fi
-
     done
-  done
+
+  else # Original script:
+   for mb in 1 3 6; do
+        for me in 1 4; do
+            if [[ $me -gt 1 ]]
+            then
+                BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-mb${mb}me4_echo-2_bold.nii.gz
+            else
+                BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-mb${mb}me1_bold.nii.gz
+            fi
+
+        #reffile=sub-${basedir)/bids/sub-${sub}/func/${sub}_task-sharedreward_acq-mb${mb}me4_echo-1_part-mag_sbref.nii.gz
+
+        # Manages the number of jobs and cores
+        OUT=${basedir}/derivatives/fsl/mcflirt/sub-${sub}/mb${mb}me${me}/
+        if [ -d $OUT ]
+        then
+            echo "sub ${sub} mb ${mb} me ${me} already has mcflirt"
+        else
+
+            mkdir -p $OUT
+
+            NCORES=15
+            while [ $(ps -ef | grep -v grep | grep 'mcflirt' | wc -l) -ge $NCORES ]; do
+                sleep 5s
+            done
+            /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
+            echo $BIDSDATA $OUT &
+                    sleep 1s
+        fi
+
+        done
+    done
+  fi
 done
 
