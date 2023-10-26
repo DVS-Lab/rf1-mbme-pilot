8a4e5c4ebde75f610973a6a27524a7f82037135b
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-26 01:23:59 -0400

    fix if statement for seq pilot 2 subs

--- code/run_motioneval.sh
+++ code/run_motioneval.sh
@@ -15,11 +15,11 @@ for sub in `ls -d ${basedir}/bids/sub-*/`; do
 
   if [ "${sub}" == "10589sp" ] || [ "${sub}" == "10590sp" ] || [ "${sub}" == "10603sp" ] || [ "${sub}" == "10606sp" ] || [ "${sub}" == "10608sp" ] || [ "${sub}" == "10644sp" ] || [ "${sub}" == "10690sp" ] || [ "${sub}" == "10691sp" ] || [ "${sub}" == "10723sp" ] || [ "${sub}" == "10741sp" ] || [ "${sub}" == "10777sp" ] || [ "${sub}" == "10803sp" ]; then
     for mbme in mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50; do
-	    if [[ $mbme -gt mb3me1fa50 ]]
+	    if [[ "$mbme" == "mb3me1fa50" ]]
 	    then
-		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-${mbme}_echo-2_bold.nii.gz
-	    else
 		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-${mbme}_bold.nii.gz
+	    else
+		BIDSDATA=${basedir}/bids/sub-${sub}/func/sub-${sub}_task-${task}_acq-${mbme}_echo-2_bold.nii.gz
 	    fi
 
 	#reffile=sub-${basedir)/bids/sub-${sub}/func/${sub}_task-sharedreward_acq-mb${mb}me4_echo-1_part-mag_sbref.nii.gz
@@ -37,7 +37,10 @@ for sub in `ls -d ${basedir}/bids/sub-*/`; do
   	    while [ $(ps -ef | grep -v grep | grep 'mcflirt' | wc -l) -ge $NCORES ]; do
     		sleep 5s
   	    done
-  	    /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
+  	    # New command: need to confirm what the reffile should be for all acqs
+            #/usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
+            # Old command:
+            /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -refvol 0 -rmsrel -rmsabs -spline_final &
             echo $BIDSDATA $OUT &
 		    sleep 1s
         fi
@@ -68,8 +71,11 @@ for sub in `ls -d ${basedir}/bids/sub-*/`; do
             while [ $(ps -ef | grep -v grep | grep 'mcflirt' | wc -l) -ge $NCORES ]; do
                 sleep 5s
             done
-            /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
-            echo $BIDSDATA $OUT &
+	    # New command: need to confirm what the reffile should be for all acqs
+            #/usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -reffile ${reffile} -rmsrel -rmsabs -spline_final &
+	    # Old command:
+            /usr/local/fsl/bin/mcflirt -in $BIDSDATA -out $OUT -mats -plots -refvol 0 -rmsrel -rmsabs -spline_final &
+	    echo $BIDSDATA $OUT &
                     sleep 1s
         fi
 
