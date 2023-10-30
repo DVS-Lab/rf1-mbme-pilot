30d6b17627c146ddb140354dd1af5f52b51fbf76
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-30 16:57:11 -0400

    update for sp subs

--- code/gen3colfiles.sh
+++ code/gen3colfiles.sh
@@ -22,7 +22,7 @@ fi
 sub=$1
 
 
-for acq in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4; do
+for acq in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4 mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50; do
   input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-sharedreward*_acq-${acq}_events.tsv
   output=${baseout}/sub-${sub}/sharedreward/acq-${acq}
   mkdir -p $output
