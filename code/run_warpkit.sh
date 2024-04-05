46e4eaa1ced5db577cba006db32d8b58afe75bcc
David V. Smith <david.v.smith@temple.edu>
2024-04-05 12:02:21 -0400

    initial commit

---
+++ code/run_warpkit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "scriptdir: ${scriptdir}"
+
+rm -rf $scriptdir/missingFiles-warpkit.log
+touch $scriptdir/missingFiles-warpkit.log
+
+for sub in `cat ${scriptdir}/sublist_all.txt` ; do
+	for acq in mb1me1 mb1me4 mb2me4 mb3me1 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50 mb6me1 mb6me4 ; do
+		script=${scriptdir}/warpkit.sh
+		NCORES=5
+		while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
+			sleep 5s
+		done
+	   bash $script $sub $acq &
+	done 
+done
---
+++ code/warpkit.sh
@@ -0,0 +1,62 @@
+#!/bash/bin
+
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+echo "scriptdir: ${scriptdir}"
+echo "maindir: ${maindir}"
+
+sub=$1
+task=sharedreward
+acq=$2
+
+outdir=$maindir/derivatives/warpkit/sub-${sub}
+if [ ! -d $outdir ]; then
+	mkdir -p $outdir
+fi
+
+indir=${maindir}/bids/sub-${sub}/func
+if [ ! -e $indir/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.json ]; then
+	echo "NO DATA: sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.json"
+	echo "NO DATA: sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.json" >> $scriptdir/missingFiles-warpkit.log
+	exit
+fi
+
+# don't re-do existing output
+if [ -e $maindir/bids/sub-${sub}/fmap/sub-${sub}_acq-${acq}_fieldmap.json ]; then
+	echo "EXISTS (skipping): sub-${sub}/fmap/sub-${sub}_acq-${acq}_fieldmap.json"
+	exit
+fi
+
+/ZPOOL/data/tools/apptainer/bin/singularity run --cleanenv \
+-B $indir:/base \
+-B $outdir:/out \
+/ZPOOL/data/tools/warpkit.sif \
+--magnitude /base/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.nii.gz \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-2_part-mag_bold.nii.gz \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-3_part-mag_bold.nii.gz \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-4_part-mag_bold.nii.gz \
+--phase /base/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-phase_bold.nii.gz \
+		/base/sub-${sub}_task-${task}_acq-${acq}_echo-2_part-phase_bold.nii.gz \
+		/base/sub-${sub}_task-${task}_acq-${acq}_echo-3_part-phase_bold.nii.gz \
+		/base/sub-${sub}_task-${task}_acq-${acq}_echo-4_part-phase_bold.nii.gz \
+--metadata /base/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-phase_bold.json \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-2_part-phase_bold.json \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-3_part-phase_bold.json \
+			/base/sub-${sub}_task-${task}_acq-${acq}_echo-4_part-phase_bold.json \
+--out_prefix /out/sub-${sub}_task-${task}_acq-${acq}
+
+# extract first volume as fieldmap and copy to fmap dir. still need json files for these. 
+fslroi $outdir/sub-${sub}_task-${task}_acq-${acq}_fieldmaps.nii $maindir/bids/sub-${sub}/fmap/sub-${sub}_acq-${acq}_fieldmap 0 1
+fslroi $indir/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.nii.gz $maindir/bids/sub-${sub}/fmap/sub-${sub}_acq-${acq}_magnitude 0 1
+
+# placeholders for json files. will need editing.
+cp $indir/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-mag_bold.json $maindir/bids/sub-${sub}/fmap/sub-${sub}_acq-${acq}_magnitude.json
+cp $indir/sub-${sub}_task-${task}_acq-${acq}_echo-1_part-phase_bold.json $maindir/bids/sub-${sub}/fmap/sub-${sub}_acq-${acq}_fieldmap.json
+
+# trash the rest
+rm -rf $outdir/sub-${sub}_task-${task}_acq-${acq}_displacementmaps.nii
+rm -rf $outdir/sub-${sub}_task-${task}_acq-${acq}_fieldmaps_native.nii
+
