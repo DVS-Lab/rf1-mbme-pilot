6875045d7b58d9379678bc625eabbe52175251c9
David V. Smith <david.v.smith@temple.edu>
2024-04-07 08:45:38 -0400

    remove clutter

--- code/warpkit.sh
+++ code/warpkit.sh
@@ -5,9 +5,6 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-echo "scriptdir: ${scriptdir}"
-echo "maindir: ${maindir}"
-
 sub=$1
 task=sharedreward
 acq=$2
